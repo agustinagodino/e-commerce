df01cd558501fb5970a5208a147b275044e72e87
agustinagodino <68866036+agustinagodino@users.noreply.github.com>
2020-09-13 00:29:45 -0300

    entrega 14-09

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,127 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+var producto = {};
+
+function showImages(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("productImages").innerHTML = htmlContentToAppend;
+    }
+}
+
+function showcomments(arrayComments){
+
+    let comments= "";
+
+
+
+    for(comment in arrayComments){
+        let puntos = "";
+        
+
+        for (let i =1 ; i<= arrayComments[comment].score; i++)    {
+            puntos +=  ` <span class="fa fa-star checked"></span> `;
+        }    
+        for (let i = arrayComments[comment].score + 1; i<=5; i ++)    {
+            puntos +=  ` <span class="fa fa-star"></span> `;
+            }
+
+        comments += `
+        <a  class="list-group-item list-group-item-action">
+            <div class="row">
+                
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <dt class="mb-1">`+ arrayComments[comment].user+`</dt>
+                        <small class="text-muted">` + arrayComments[comment].dateTime + ` </small>
+                    </div>
+                    <p class="mb-1">` +arrayComments[comment].description  + `</p>
+                    <p class="mb-1">` + puntos + `</p>
+                </div>
+            </div>
+        </a>
+        `
+        
+      
+        }
+
+
+        document.getElementById("productcoment").innerHTML = comments ;
+    }
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostHTML = document.getElementById("productCount");
+            let productCrHTML = document.getElementById("productCriteria");
+        let productSoldHTML = document.getElementById("productSold");
+        let productCatHTML = document.getElementById("productCat")
+
+           productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.cost;
+            productCrHTML.innerHTML = product.currency;
+
+            productSoldHTML.innerHTML = product.soldCount;
+            productCatHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            showImages(product.images);
+        }
+    });
 });
+
+
+
+
+
+
+
+
+
+    document.addEventListener("DOMContentLoaded", function (e) {
+        getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok"){
+                comment = resultObj.data;
+               showcomments(comment);
+            }
+        });
+
+        getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
+            if (resultObj.status === "ok") {
+                product = resultObj.data;
+
+                
+            }});
+
+          
+        });
+
+
+
+
+
+
+
+        
--- js/products.js
+++ js/products.js
@@ -69,9 +69,12 @@ function showProductsList(array) {
 htmlContentToAppend += `
         <a href="product-info.html" class="list-group-item list-group-item-action">
             <div class="row">
+
+            
                 <div class="col-3">
                     <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                 </div>
+
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                     <h4 class="mb-1"> `+ product.name +`</h4>
@@ -82,6 +85,9 @@ htmlContentToAppend += `
                 <br>
                 <h4 class="mb-1"> `+ product.currency + ` `+ product.cost + `</h4>
             </div>
+
+            
+
         </div>
         </a>
         `
@@ -90,6 +96,8 @@ htmlContentToAppend += `
        
  
     }
+
+   
 }
 
 
