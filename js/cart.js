cf0cc1e17de0d38f42b9068323adf15b281dd8ee
agustinagodino <68866036+agustinagodino@users.noreply.github.com>
2020-10-11 16:05:35 -0300

    Cambios carrito

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,78 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+var carritoArray = [];
+
+
+function calcTotal(){
+    let total = 0;
+    let subs = document.getElementsByClassName("subtotal");
+    for (let i = 0; i < subs.length; i++){
+        total += parseInt(subs[i].innerHTML);
+    }
+    document.getElementById("total").innerHTML = total;
+    calcEnvio()
+}
+
+
+function calcSubtotal(precio, i){
+
+    let count = parseInt(document.getElementById(`count${i}`).value);
+    subtotal = count * precio;      
+    document.getElementById(`carroSubtotal${i}`).innerHTML = subtotal;
+    calcTotal();
+}
+
+
+function showCarrito(array) {
+
+    let contenido = "";
+
+    for (let i = 0; i < array.length; i++) {
+        
+        let carro = array[i];
+
+        let sub = carro.unitCost * carro.count;
+
+        contenido += `
+        <tr>
+            <td><img src='${carro.src}' width="50px"></td>
+
+            <td>${carro.name}</td>
+            <td>${carro.currency}</td>
+
+            <td>${carro.unitCost}</td>
+
+            <td><input style="width:60px;" onchange="calcSubtotal(${carro.unitCost}, ${i})" 
+                type="number" id="count${i}" value="${carro.count}" min="1"></td>
+
+            <td><span class="subtotal" id="carroSubtotal${i}" style="font-weight:bold;">${sub}</span></td>
+        </tr>
+        `
+
+        document.getElementById("listado").innerHTML = contenido;
+    }
+    calcTotal();
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            carritoArray = resultObj.data.articles;
+
+            
+            showCarrito(carritoArray);
+
+            
+
+        }
+
+    });
+
+    
+
 
 });
